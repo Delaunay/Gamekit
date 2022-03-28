ec2627846226b0be6573b4102c475f826488fbf9
setepenre <setepenre@outlook.com>
2022-03-27 20:46:26 -0400

    Tests are recognized

--- RunTests.bat
+++ RunTests.bat
@@ -2,13 +2,14 @@
 
 
 set Plugins=C:\Users\Archimedes\work\gamekit\Plugins\Gamekit\
+set ProjectFolder=C:\Users\Archimedes\work\gamekit\
 set Project=C:\Users\Archimedes\work\gamekit\Chessy.uproject
 set Map=/Game/Levels/FlatLandscape.umap
-set Args=-log -Unattended -NullRHI -NoSplash -NoSound -NoPause -Messaging
-set TestArgs=ABSLOG="Saved/Automation/log.txt" -ReportOutputPath="Saved/Automation/Report" -TestExit="Automation Test Queue Empty"
+set Args=-log -Unattended -NullRHI -NoSplash -NoSound -NoPause
+set TestArgs=ABSLOG="%ProjectFolder%/Saved/Automation/log.txt" -ReportOutputPath="%ProjectFolder%/Saved/Automation/Report" -TestExit="Automation Test Queue Empty"
 
 set UEPATH=C:\opt\UnrealEngine\Engine\Binaries\Win64\UE4Editor.exe
 set UATPATH=C:\opt\UnrealEngine\Engine\Build\BatchFiles\RunUAT.bat
 
-%UEPATH% %Project% %Map% %Args% %TestArgs% -ExecCmds="Automation RunTests Gamekit.*"
+%UEPATH% %Project% %Map% %Args% %TestArgs% -ExecCmds="Automation RunTests Gamekit"
 
