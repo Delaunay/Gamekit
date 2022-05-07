6763c563f900512b4d00138859da54b77ecb30ce
setepenre <setepenre@outlook.com>
2022-05-06 22:26:46 -0400

    Add docs cookiecutter command

--- Source/Python/gamekit/commands/docs.py
+++ Source/Python/gamekit/commands/docs.py
@@ -3,6 +3,10 @@ import pkg_resources
 
 from gamekit.conf import load_conf, save_conf, CONFIG, CONFIGNAME, Command
 
+from cookiecutter.main import cookiecutter
+
+
+COOKIECUTTER = "https://github.com/Delaunay/UEDocs"
 
 class Docs(Command):
     """Add a docs folder to your project"""
@@ -18,7 +22,13 @@ class Docs(Command):
 
     @staticmethod
     def execute(args):
-        pass
+        project = args.project
+
+        projects_folder = load_conf().get('project_path')
+        project_folder = os.path.join(projects_folder, project)
+
+        os.chdir(project_folder)
+        cookiecutter(COOKIECUTTER)
 
 
 COMMAND = Docs
