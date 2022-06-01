c4b339bc4e1e5cefaf3317fe5bae8561c319dc72
Setepenre <pierre.delaunay@outlook.com>
2022-06-01 19:06:46 -0400

    Fully Generated GA, using base blueprint as parent #68

--- Script/Abilities/generate_abilities.py
+++ Script/Abilities/generate_abilities.py
@@ -3,13 +3,45 @@ import unreal
 default_datatable = "/Gamekit/Data/AbilityData"
 default_destination = '/Gamekit/Abilities/Generated'
 
-#  from importlib import reload
-#  import Abilities.generate_abilities as gkgen
-#  reload(gkgen)
-#  gkgen.generate_gamekit_abilities()
+
+def generate_ability(root, name, table, parent):
+    """Create a new ability from a datatable"""
+    bpfactory = unreal.BlueprintFactory()
+    bpfactory.set_editor_property('parent_class', parent)
+    bpfactory.set_editor_property('edit_after_new', False)
+    bpfactory.set_editor_property('supported_class', unreal.Blueprint)
+
+    asset_name = f'GA_{name}'
+
+    # Create the basic blueprint
+    ability = unreal.AssetToolsHelpers.get_asset_tools().create_asset(
+        asset_name=asset_name,
+        package_path=root,
+        asset_class=None,
+        factory=bpfactory
+    )
+
+    # Save the blueprint (to compile it)
+    unreal.EditorAssetLibrary.save_loaded_asset(ability)
+
+    # Get the Generated class
+    bp_gc = unreal.load_object(None, root + '/' + f'{asset_name}.{asset_name}_C')
+
+    # Get the CDO of the class
+    bp_cdo = unreal.get_default_object(bp_gc)
+
+    # Set the properties
+    bp_cdo.set_editor_property("AbilityDatatable", table)
+    bp_cdo.set_editor_property("AbilityRowName", name)
+
 
 def generate_abilities_from_table(datatable, package):
     """Generate a new Gameplay ability for every row of a given datatable"""
+    #  import Abilities.generate_abilities as gkgen
+    #  reload(gkgen); gkgen.generate_abilities_from_table()
+
+    parent_obj = unreal.load_object(None, '/Gamekit/Abilities/GAC_AbilityBase_Prototype.GAC_AbilityBase_Prototype_C')
+    parent_cls = unreal.get_default_object(parent_obj).get_class()
 
     table = unreal.EditorAssetLibrary.load_asset(datatable)
 
@@ -18,27 +50,21 @@ def generate_abilities_from_table(datatable, package):
         asset_path = package + '/' + name
 
         if not unreal.EditorAssetLibrary.does_asset_exist(asset_path):
-            print(name)
-
-            ability = unreal.AssetToolsHelpers.get_asset_tools().create_asset(
-                asset_name=f'GA_{name}',
-                package_path=package,
-                asset_class=unreal.GKGameplayAbility,
-                factory=unreal.GKGameplayAbilityFactory()
-            )
-
-            # Set their config
-            ability.ability_data_table = table
-            ability.ability_row_name = fname
+            generate_ability(package, name, table, parent_cls)
+        else:
+            print(f'Skipping {asset_path}')
 
 
 def generate_gamekit_abilities():
+    #  import Abilities.generate_abilities as gkgen
+    #  reload(gkgen); gkgen.generate_gamekit_abilities()
     generate_abilities_from_table(default_datatable, default_destination)
 
 
-# ga = unreal.AssetToolsHelpers.get_asset_tools().create_asset(
-#     asset_name=f'GA_Test',
-#     package_path=package,
-#     asset_class=unreal.GKGameplayAbility,
-#     factory=unreal.GKGameplayAbilityFactory()
-# )
+def generate_test():
+    #  from importlib import reload
+    #  import Abilities.generate_abilities as gkgen
+    #  reload(gkgen); gkgen.generate_test()
+
+    package = '/Gamekit/Abilities/Generated'
+    generate_ability(package, "Test", None)
